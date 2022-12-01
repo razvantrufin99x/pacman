7a132a5ce2eaa79af16060d726d0b1584b035dd9
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-01 23:56:16 +0200

    Add project files.

---
+++ pacman/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace pacman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.nava = new System.Windows.Forms.Button();
+            this.userControl11 = new pacman.UserControl1();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(22, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "<";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(38, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(23, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "A";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(38, 67);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(23, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "V";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(65, 35);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(22, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = ">";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // nava
+            // 
+            this.nava.Location = new System.Drawing.Point(134, 127);
+            this.nava.Name = "nava";
+            this.nava.Size = new System.Drawing.Size(20, 20);
+            this.nava.TabIndex = 4;
+            this.nava.Text = "+";
+            this.nava.UseVisualStyleBackColor = true;
+            // 
+            // userControl11
+            // 
+            this.userControl11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.userControl11.Location = new System.Drawing.Point(120, 110);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(160, 160);
+            this.userControl11.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 453);
+            this.Controls.Add(this.nava);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.userControl11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button nava;
+        private UserControl1 userControl11;
+    }
+}
+
---
+++ pacman/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace pacman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int[,] harta = 
+        {
+        {1,1,1,1,1,1,1,1},
+        {1,0,0,0,1,0,0,1},
+        {1,1,1,0,1,0,0,1},
+        {1,0,0,0,1,0,0,1},
+        {1,1,1,0,0,0,1,1},
+        {1,0,0,0,1,0,0,1},
+        {1,0,0,0,1,0,0,1},
+        {1,1,1,1,1,1,1,1},
+        };
+
+        int i = 1;
+        int j = 1;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (harta[ j-1,i] != 1)
+            {
+                nava.Top -= 20;
+                j--;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (harta[ j+1,i] != 1)
+            {
+                nava.Top += 20;
+                j++;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (harta[j,i-1] != 1)
+            {
+                nava.Left -= 20;
+                i--;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (harta[j,i+1] != 1)
+            {
+                nava.Left += 20;
+                i++;
+            }
+        }
+    }
+}
